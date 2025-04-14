0d467817be7bfb97a7d931a812d8355ad6081ef2
David <cordondavid532@gmail.com>
2025-04-14 14:08:04 -0500

    feat: Create the AcademicResourceDTO class

---
+++ src/main/java/co/edu/udes/backend/dto/AcademicResourceDTO.java
@@ -0,0 +1,15 @@
+package co.edu.udes.backend.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AcademicResourceDTO {
+    private long id;
+    private String name;
+    private String category;
+    private boolean availability;
+}
