7bcedc2a532b8f230cf815dfc3fc659bb833a7fe
JoseMartInez2611 <JoseMartinez2611@proton.me>
2025-04-16 15:50:00 -0500

    Fix: re-structuring the variables from some data on the DTO and adding the @Mapping on the classes AcademicRecordMapper, AcademicRegistrationMapper, FinalNoteMapper.java

--- src/main/java/co/edu/udes/backend/mappers/AcademicRegistrationMapper.java
+++ src/main/java/co/edu/udes/backend/mappers/AcademicRegistrationMapper.java
@@ -3,6 +3,7 @@ package co.edu.udes.backend.mappers;
 import co.edu.udes.backend.dto.AcademicRegistrationDTO;
 import co.edu.udes.backend.models.AcademicRegistration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -11,9 +12,15 @@ import java.util.List;
 public interface AcademicRegistrationMapper {
     AcademicRegistrationMapper INSTANCE = Mappers.getMapper(AcademicRegistrationMapper.class);
 
+
+    @Mapping(source = "studentId", target = "student.id")
+    @Mapping(source = "groupId", target = "group.id")
     AcademicRegistration toEntity(AcademicRegistrationDTO academicRegistration);
     List<AcademicRegistration> toEntityList(List<AcademicRegistrationDTO> academicRegistrations);
 
+
+    @Mapping(source = "student.id", target = "studentId")
+    @Mapping(source = "group.id", target = "groupId")
     AcademicRegistrationDTO toDto(AcademicRegistration academicRegistration);
     List<AcademicRegistrationDTO> toDtoList(List<AcademicRegistration> academicRegistrations);
 }
