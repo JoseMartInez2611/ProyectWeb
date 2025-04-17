54e3ee05d21f8d7af88df6f7530f3280f6083181
JoseMartInez2611 <JoseMartinez2611@proton.me>
2025-04-16 21:46:55 -0500

    fix: adding the @Mapping on the careerMapper

--- src/main/java/co/edu/udes/backend/mappers/CareerMapper.java
+++ src/main/java/co/edu/udes/backend/mappers/CareerMapper.java
@@ -2,10 +2,12 @@ package co.edu.udes.backend.mappers;
 
 import co.edu.udes.backend.dto.CareerDTO;
 import co.edu.udes.backend.models.Career;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
+@Mapper(componentModel = "spring")
 public interface CareerMapper {
     CareerMapper INSTANCE = Mappers.getMapper(CareerMapper.class);
 
