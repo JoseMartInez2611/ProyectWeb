3df1d6d1a02aeed3939863fe93d7cd4bcbb67cf8
JoseMartInez2611 <JoseMartinez2611@proton.me>
2025-04-19 19:33:13 -0500

    fix: removed unused imports

--- src/main/java/co/edu/udes/backend/repositories/RoomRepository.java
+++ src/main/java/co/edu/udes/backend/repositories/RoomRepository.java
@@ -2,11 +2,8 @@ package co.edu.udes.backend.repositories;
 
 import co.edu.udes.backend.models.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
