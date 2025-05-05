a846a863ddb4e35d0240151bb6f41a2564a8cdcc
JoseMartInez2611 <JoseMartinez2611@proton.me>
2025-05-05 00:04:42 -0500

    fix: removed unused imports

--- src/main/java/co/edu/udes/backend/repositories/QualificationCategoryRepository.java
+++ src/main/java/co/edu/udes/backend/repositories/QualificationCategoryRepository.java
@@ -3,7 +3,6 @@ package co.edu.udes.backend.repositories;
 import co.edu.udes.backend.models.QualificationCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
